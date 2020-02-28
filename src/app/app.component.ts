ce239c9a96a50e1cd11f7cd5c26208f336c60ea9
Kelly Davidson <kellyadavidsonuk@gmail.com>
2020-02-28 18:56:46 +0000

    basic random password functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,14 @@ export class AppComponent {
   letters = false;
   numbers = false;
   symbols = false;
+  length = 0;
+
+  useLength(value: string) {
+    const parsedNum = parseInt(value);
+    if (!isNaN(parsedNum)) {
+      this.length = parsedNum;
+    }
+  }
 
   useLetters() {
     this.letters = !this.letters;
@@ -24,7 +32,27 @@ export class AppComponent {
   }
 
   onButtonClick() {
-    console.log(`${this.letters}, ${this.numbers}, ${this.symbols}`);
-    this.password = "my password";
+    const numbers = "1234567890";
+    const letters = "abcdefghijklmnopqrstuvwxyz";
+    const symbols = "!@£$%^&*()";
+
+    let validChars = "";
+
+    if (this.letters) {
+      validChars += letters;
+    }
+    if (this.numbers) {
+      validChars += numbers;
+    }
+    if (this.symbols) {
+      validChars += symbols;
+    }
+
+    let genPass = "";
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      genPass += validChars[index];
+    }
+    this.password = genPass;
   }
 }
